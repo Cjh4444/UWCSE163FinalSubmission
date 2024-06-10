f2ea9f6b0103111c433e23165de87ab63e0034d6
Camden Harris <camdenharris@gmail.com>
2024-06-09 22:50:04 -0700

    wrote readme and added comments to scraping file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # UWCSE163FinalSubmission
+
+Steps to Run
+
+Libraries Required
+
+* Pandas
+* Matplotlib
+* Seaborn
+* Requests
+* Dotenv
+
+Steps to Run
+
+(Optional)
+
+1. Create Spotify developer account
+2. Create Application
+3. Grab client ID and secret key, place in .env file
+4. Run "spotify_scraping.py" to grab data for all 1000 songs (will take ~20 minutes if you don't get banned
+
+For Analysis
+
+1. Run "analysis.py", image outputs will be put in "output" folder
--- final submission/spotify_scraping.py
+++ final submission/spotify_scraping.py
@@ -13,6 +13,11 @@ CLIENT_SECRET: str = os.getenv("CLIENT_SECRET")
 
 
 def get_token():
+    """
+    Grabs session token from Spotify API using client id and secret key
+    Returns:
+        str: token for api authentication
+    """
     auth_string = CLIENT_ID + ":" + CLIENT_SECRET
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
@@ -30,10 +35,30 @@ def get_token():
 
 
 def get_auth_header(token: str):
+    """
+    Converts session token to header to be used for API calls
+
+    Args:
+        token (str): session token
+
+    Returns:
+        str: header string
+    """
     return {"Authorization": "Bearer " + token}
 
 
 def search_for_artist(token, artist_name):
+    """
+    Searches Spotify's database and returns the artist id associated
+    with the provided name
+
+    Args:
+        token (str): session token
+        artist_name (str): name of artist to search for
+
+    Returns:
+        str: Spotify id for given artist
+    """
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
     query = f"?q={artist_name}&type=artist&limit=1"
@@ -50,29 +75,17 @@ def search_for_artist(token, artist_name):
     return json_result[0]
 
 
-def get_artist_albums(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/albums"
-    headers = get_auth_header(token)
-    result = requests.get(url, headers=headers)
-    json_result = json.loads(result.content)
-    albums = json_result["items"]
-    album_ids = [album["id"] for album in albums]
-
-    return album_ids
-
-
-def get_album_songs(token, album_id):
-    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
-    headers = get_auth_header(token)
-    result = requests.get(url, headers=headers)
-    json_result = json.loads(result.content)
-    songs = json_result["items"]
-    song_names = [song["id"] for song in songs]
-
-    return song_names
-
-
 def get_artist_top_songs(token, artist_id):
+    """
+    Returns the top 10 tracks for a given artist
+
+    Args:
+        token (str): session token
+        artist_id (str): Spotify artist ID
+
+    Returns:
+        list[dict]: list of 10 songs with relevant statistics
+    """
     url = (
         f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     )
@@ -84,19 +97,21 @@ def get_artist_top_songs(token, artist_id):
     return json_result
 
 
-def get_all_songs_by_artist(token, artist_id) -> list:
-    return [
-        song
-        for album_id in get_artist_albums(token, artist_id)
-        for song in get_album_songs(token, album_id)
-    ]
-
-
 def get_track_features(token, track_id) -> dict:
+    """
+    Returns track features of a given song
+
+    Args:
+        token (str): session token
+        track_id (str): Spotify track ID
+
+    Returns:
+        dict: dictionary all track features of a song
+    """
     url = f"https://api.spotify.com/v1/audio-features/{track_id}"
     headers = get_auth_header(token)
     result = requests.get(url, headers=headers)
-    print(result.headers)
+    # print(result.headers)
     json_result: dict = json.loads(result.content)
     json_result.pop("uri")
     json_result.pop("track_href")
@@ -106,6 +121,16 @@ def get_track_features(token, track_id) -> dict:
 
 
 def get_track_analysis(token, track_id) -> dict:
+    """
+    Returns track analysis of a given song
+
+    Args:
+        token (str): session token
+        track_id (str): Spotify track ID
+
+    Returns:
+        dict: dictionary all track analysis of a song
+    """
     url = f"https://api.spotify.com/v1/audio-analysis/{track_id}"
     headers = get_auth_header(token)
     result = requests.get(url, headers=headers)
@@ -143,23 +168,22 @@ def get_track_analysis(token, track_id) -> dict:
 
 
 def main():
+    """
+    Searches for and creates a csv of all top 100 artist ids,
+    and then creates a csv of the top 1000 songs with all
+    relevant details for analysis
+    """
     token = get_token()
 
-    # with open("artists.csv") as f:
-    #     top_1000_songs = []
-    #     for artist in f:
-    #         artist_id = search_for_artist(token, artist.strip())["id"]
-    #         for song in get_artist_top_songs(token, artist_id):
-    #             top_1000_songs.append(song["id"])
-    #         print(f"{artist.strip()} completed")
-    #     df = pd.DataFrame(top_1000_songs)
-    #     df.to_csv("top1000songs.csv", index=False)
-
-    # artist_name = "Rihanna"
-    # artist_id = search_for_artist(token, artist_name)["id"]
-    # print(f"{artist_name=}")
-    # print(f"{artist_id=}")
-    # print(f"{get_artist_top_songs(token, "1Xyo4u8uXC1ZmMpatF05PJ")=}")
+    with open("artists.csv") as f:
+        top_1000_songs = []
+        for artist in f:
+            artist_id = search_for_artist(token, artist.strip())["id"]
+            for song in get_artist_top_songs(token, artist_id):
+                top_1000_songs.append(song["id"])
+            print(f"{artist.strip()} completed")
+        df = pd.DataFrame(top_1000_songs)
+        df.to_csv("top1000songs.csv", index=False)
 
     with open("top1000songs.csv") as f:
         output = []
@@ -173,12 +197,6 @@ def main():
             if (idx % 100) == 0:
                 df = pd.DataFrame(output)
                 df.to_csv("1000songdata.csv", index=False)
-
-        df = pd.DataFrame(output)
-        df.to_csv("1000songdata.csv", index=False)
-
-        df = pd.DataFrame(output)
-        df.to_csv("1000songdata------2.csv", index=False)
 
 
 if __name__ == "__main__":
